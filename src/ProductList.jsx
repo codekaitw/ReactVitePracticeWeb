e63e7af109d143eab1a3be53bd2cc360f905fa6a
codekaitw <65647662+codekaitw@users.noreply.github.com>
2023-09-22 00:32:37 -0400

    fetch data, using useEffect

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,65 +1,40 @@
-import styles from './ProductList.module.css';
-import {Link} from 'react-router-dom';
-import Title from './Title';
+import styles from "./ProductList.module.css";
+import { Link } from "react-router-dom";
+import Title from "./Title";
+import { useState, useEffect } from "react"; // react hook
 
 export default function ProductList() {
-  let productList = [
-    {
-      id: 1,
-      name: "Apple",
-      price: 5,
-      image: "apple.jpeg",
-      description: "An Apple a day keeps the doctor away",
-    },
-    {
-      id: 2,
-      name: "Banana",
-      price: 4,
-      image: "banana.jpeg",
-      description: "Bananas are good for you",
-    },
-    {
-      id: 3,
-      name: "Peach",
-      price: 19,
-      image: "peach.jpeg",
-      description: "Peach is a good fruit",
-    },
-    {
-      id: 4,
-      name: "Watermelon",
-      price: 20,
-      image: "watermelon.png",
-      description: "Juicy watermelon is here for you",
-    },
-    {
-      id: 5,
-      name: "Kiwi",
-      price: 13,
-      image: "kiwi.jpeg",
-      description: "Kiwi have lots of health benefits",
-    },
-  ];
+  let [productList, setProductList] = useState([]);
+
+  // react effect: first callback to set trigger condition
+  useEffect(() => {
+    // 1: if no second argument, it will run every time component re-render(change useState), it will trigger this callback
+    // 2: <common use to fetch data>if second argument(Dependency Array) is empty array, it will run(render) only once, it will not trigger this callback
+    // 3: <use to detect parameter change>if second argument(Dependency Array) is not empty array(have parameter), it will re-render when parameter changed.
+    fetch(import.meta.env.BASE_URL + 'react-basic-product.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setProductList(data);
+      });
+  }, []);
 
   return (
     <div>
-      <Title mainTitle="Please choose fruits to buy"/>
-      {productList.map(product =>
-        (
+      <Title mainTitle="Please choose fruits to buy" />
+      {productList.map((product) => (
         <div className={styles.productBorder} key={product.id}>
           {product.name}
           <br />
           {product.price}
           <br />
-          <Link to={import.meta.env.BASE_URL + 'Product/' + product.id}>
-          <img src={import.meta.env.BASE_URL + '/img/' + product.image}></img>
+          <Link to={import.meta.env.BASE_URL + "Product/" + product.id}>
+            <img src={import.meta.env.BASE_URL + "/img/" + product.image}></img>
           </Link>
           <br />
           {product.description}
           <br />
         </div>
-        )
-      )}
+      ))}
     </div>
   );
 }
